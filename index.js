63e3e0e8f9ba168f48bc05e8399a65ad9cddd187
SOWNDARYA R <107302244+sowndary-r@users.noreply.github.com>
2023-03-05 12:29:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+
+
+
+function clock()
+{
+    let ampm=document.getElementById('ampm');
+    let datetime=new Date();
+    let d=datetime.getDay();
+     let day='';
+   switch(d)
+   {
+        case 0:
+        day="Sunday";
+        break;
+        case 1:
+        day="Monday";
+        break;
+        case 2:
+        day="Tuesday";
+        break;
+        case 3:
+        day="Wednesday";
+        break;
+        case 4:
+        day="Thursday";
+        break;
+        case 5:
+        day="Friday";
+        break;
+        case 6:
+        day='Saturday';
+        break;
+   }
+    console.log(d);
+    document.getElementById('day').innerHTML=day;
+    let date=datetime.getDate();
+    document.getElementById('dat').innerHTML=date;
+    let mon=datetime.getMonth();
+    document.getElementById('mon').innerHTML=mon+1;
+    let year=datetime.getFullYear();
+    document.getElementById('year').innerHTML=year;
+
+    let hr=datetime.getHours();
+    if(hr>=12)
+    ampm.innerHTML="PM";
+    if(hr>12)
+    {
+        hr=hr-12;
+    }
+    document.getElementById('hr').innerHTML=padzero(hr);
+    let min=datetime.getMinutes();
+    document.getElementById('min').innerHTML=padzero(min);
+    let sec=datetime.getSeconds();
+    document.getElementById('sec').innerHTML=padzero(sec);
+}
+function padzero(num)
+{
+  return num<10?"0"+num:num;
+}
+setInterval(clock,300);
+
